cca9007b90bd849492654084a43476a95054658f
nick1392 <niccolo.bisagno@unitn.it>
2021-01-30 00:41:50 +0100

    menu + game ending condition

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("I'm quitting the hell out of this shitty game");
+        Application.Quit();
+    }
+}
