656b6f3384896ab6327e74d2da0296e7b37ddad8
A7ocin <mr_nico@hotmail.it>
2021-01-29 23:05:30 +0100

    :alien: Networking prototype

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeardedManStudios.Forge.Networking.Generated;
+
+public class MovePlayer : MovePlayerBehavior
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!networkObject.IsServer){
+            transform.position = networkObject.position;
+            return;
+        }
+
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"),
+            0
+        ) * Time.deltaTime * 5.0f;
+
+        networkObject.position = transform.position;
+    }
+}
