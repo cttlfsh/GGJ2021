36b1412dfa69d1618ff27e2acb0a607d0463be8e
Andrea Montagner <andrea.montagner@unitn.it>
2021-01-31 01:13:17 +0100

    ✔️ "Random spawn player"

--- Assets/Scripts/ProceduralGenerator.cs
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -10,8 +10,12 @@ public class ProceduralGenerator : MonoBehaviour {
     public GameObject[] terrains;
     public GameObject island;
     public GameObject mold;
+    public GameObject myMan;
+    public GameObject myGal;
+
     private float nextXMoldPosition;
     private float nextZMoldPosition;
+    private List<GameObject> instantiatedTiles = new List<GameObject>();
 
     void Awake() {
         // Basta la X dato che la tile e' quadrata
@@ -36,7 +40,7 @@ public class ProceduralGenerator : MonoBehaviour {
 
         int seed = 0;
 
-       for (int i = 0; i < rows; i++)
+        for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {   
@@ -46,12 +50,39 @@ public class ProceduralGenerator : MonoBehaviour {
                 // int r = Random.Range(0, terrains.Length);
                 GameObject currentTerrain = terrains[seed];
 
-                seed++;
 
-               Instantiate(currentTerrain, new Vector3(nextXMoldPosition, 0, nextZMoldPosition), Quaternion.identity);
+                GameObject ithTile = Instantiate(currentTerrain, new Vector3(nextXMoldPosition, 0, nextZMoldPosition), Quaternion.identity);
+                if (ithTile.tag != "Dungeon") {
+                    instantiatedTiles.Add(ithTile);
+                }
+                seed++;
                 
             }
         }
+
+        // FINISHED GENERATING MAP
+        // START SPAWNING DEI PERSONAGGI
+
+        int r = Random.Range(0, terrains.Length);
+        GameObject spawnTerrain = instantiatedTiles[r];
+        Debug.Log(spawnTerrain);
+
+
+        if (Random.Range(0, 2) == 0){
+            Debug.Log("SPAWN 1: " + spawnTerrain.transform.Find("SpawnMyMan1"));
+            Debug.Log("TERRAIN: " + spawnTerrain.transform.position);
+            Debug.Log("OFFSET: " + spawnTerrain.transform.Find("SpawnMyMan1").position);
+            Debug.Log(spawnTerrain.transform.position + spawnTerrain.transform.Find("SpawnMyMan1").position);
+            // Transform playerSpawn = spawnTerrain.transform.Find("SpawnMyMan1");
+            myMan.transform.position = spawnTerrain.transform.position + spawnTerrain.transform.Find("SpawnMyMan1").localPosition;
+        } else {
+            Debug.Log("SPAWN 2: " + spawnTerrain.transform.Find("SpawnMyMan2"));
+            Debug.Log(spawnTerrain.transform.position + spawnTerrain.transform.Find("SpawnMyMan2").position);
+            // Transform playerSpawn = spawnTerrain.transform.transform.Find("SpawnMyMan2");
+            myMan.transform.position = spawnTerrain.transform.position + spawnTerrain.transform.Find("SpawnMyMan2").localPosition;
+        }
+        myMan.SetActive(true);
+
 
         // maxClouds = Random.Range(500, 1000);
         // generateClouds();
